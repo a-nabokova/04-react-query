4e672cea294b1b6c418f99a466429a9a654f423f
a-nabokova <itcw2s@gmail.com>
2025-09-13 18:42:37 +0300

    useQuery, paginate

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import css from './App.module.css';
 import SearchBar from '../SearchBar/SearchBar';
 import type { Movie } from '../../types/movie';
@@ -8,45 +8,49 @@ import toast, { Toaster } from 'react-hot-toast';
 import MovieModal from '../MovieModal/MovieModal';
 import Loader from '../Loader/Loader';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import { useQuery, keepPreviousData } from '@tanstack/react-query';
+import ReactPaginate from 'react-paginate';
 
 
  
 function App() {
 
-  const [movies, setMovies] = useState<Movie[]>([]); 
-  const [error, setError] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+   
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+ const [searchValue, setSearchValue] = useState('');
+  const [currentPage, setCurrentPage] = useState(1);
 
-  const closeModal = () => {
-    setIsModalOpen(false);
-    setSelectedMovie(null);  
-  };
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['movies', searchValue, currentPage ],
+    queryFn: () => fetchMovies(searchValue, currentPage ),
+    enabled: searchValue !== "",
+    placeholderData: keepPreviousData,
+    });
+  
+   useEffect(() => {
+    if (data && data.results.length === 0) {
+      toast.error('No movies found for your request.');
+    }
+  }, [data]);
+
+  const totalPage = data?.total_pages || 0;
+
+   const handleMovieSearch = async (searchValue: string) => {
+    setSearchValue(searchValue)
+    setCurrentPage(1)
+  }
 
   const handleSelectedMovie = (movie: Movie) => {
     setSelectedMovie(movie);
     setIsModalOpen(true);  
   };
 
-  const handleMovieSearch = async (searchValue: string) => {
-    setMovies([]);
-    setError(false); 
-     setIsLoading(true);
-     try {
-       const resMovies = await fetchMovies(searchValue); 
-       if (resMovies.length === 0) {
-          toast.error('No movies found for your request.');
-           return;
-        }
-     
-     setMovies(resMovies); 
- } catch  {
-   setError(true);
-     } finally {
-       setIsLoading(false);
- }
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedMovie(null);  
   };
+
  
   return (
     <>
@@ -55,8 +59,21 @@ function App() {
 
         {isLoading && <Loader />}
         {error && !isLoading && <ErrorMessage />}
+         {data && totalPage > 1 && (
+        <ReactPaginate
+          pageCount={totalPage}
+        pageRangeDisplayed={5}     marginPagesDisplayed={1}
+          onPageChange={({ selected }) => setCurrentPage(selected + 1)}
+          forcePage={currentPage - 1}
+          containerClassName={css.pagination}
+          activeClassName={css.active}
+          nextLabel="→"
+          previousLabel="←"
+        />
+      )}
+
         {!isLoading && !error && (
-        <MovieGrid movies={movies} onSelect={handleSelectedMovie} />
+        <MovieGrid movies={data?.results || []} onSelect={handleSelectedMovie} />
       )}
          
         {isModalOpen && selectedMovie && (
--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -5,13 +5,16 @@ import type { Movie } from "../types/movie";
 
 interface FetchMoviesResponse {
   results: Movie[];
+  page: number;
+  total_pages: number;
  }
 
 
-export default async function fetchMovies (searchValue: string)  {
+export default async function fetchMovies (searchValue: string, page: number)  {
      const res = await axios.get<FetchMoviesResponse>('https://api.themoviedb.org/3/search/movie', {
       params: {
-        query: searchValue,
+         query: searchValue,
+        page,
       }, headers: {
         Authorization: `Bearer ${API_KEY}`,
          
@@ -19,6 +22,6 @@ export default async function fetchMovies (searchValue: string)  {
     });
      
 
-    return res.data.results;
+    return res.data;
   }
 
